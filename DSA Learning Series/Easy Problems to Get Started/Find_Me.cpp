50686edb24869010368e58ea06aa4d52a39737ab
Ayush Bansal <bayush611@gmail.com>
2021-07-04 12:15:07 +0530

    FINDMELI

---
+++ DSA Learning Series/Easy Problems to Get Started/Find_Me.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,k;
+    cin>>n>>k;
+
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    for(int i=0;i<n;i++){
+        if(a[i] == k){
+            cout<<"1";
+            return 0;
+        }
+    }
+    cout<<"-1";
+}
