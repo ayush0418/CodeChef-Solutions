0326b8a30d1e7403e92f4fd015034e5dc4d78f3c
Ayush Bansal <bayush611@gmail.com>
2021-07-07 14:36:47 +0530

    ZCO14003

--- DSA Learning Series/Complexity Analysis + Basics Warm Up/Smart_Phone.cpp
+++ DSA Learning Series/Complexity Analysis + Basics Warm Up/Smart_Phone.cpp
@@ -2,16 +2,10 @@
 #include<algorithm>      //  --> sort function...
 using namespace std;
 
-void show(int a[], int array_size){
-    for (int i = 0; i < array_size; ++i)
-        cout << a[i] << " ";
-}
-
 int main(){
     int n;
     cin>>n;
-    int a[n];
-    int m =__LONG_MAX__;
+    long long int a[n];
 
     for(int i=0;i<n;i++){
         cin>>a[i];
@@ -22,6 +16,5 @@ int main(){
         a[i] = a[i]*(n-i);
     }
     sort(a,a+n);
-    cout<<a[n-1]<<endl;
-
+    cout<<a[n-1];
 }
