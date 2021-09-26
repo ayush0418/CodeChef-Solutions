92e013c5ff848efb7b61d628e81d22a2573dce56
Ayush Bansal <bayush611@gmail.com>
2021-09-26 20:11:30 +0530

    The Preparations

---
+++ CodeChef Starters 13/SUPCHEF.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int m,n,k;
+        cin>>m>>n>>k;
+
+        if(m > n*k){
+            cout<<"yes"<<endl;
+        }
+        else{
+            cout<<"no"<<endl;
+        }
+
+
+    }
+}
