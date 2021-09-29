4efdf9cf2ffdc578da880ff3f9ea7347cfdb0b42
Ayush Bansal <bayush611@gmail.com>
2021-09-29 23:17:48 +0530

    Diagonal movement

---
+++ CodeChef Starters 14/FIXFIX.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+
+        if(n-k == 1){
+            cout<<"-1\n";
+        } 
+        else if(n==k){
+            for(int i=1;i<=n;i++){
+                cout<<i<<" ";
+            }
+            cout<<"\n";
+        }
+        else{
+            for(int i=1;i<=k;i++){
+                cout<<i<<" ";
+            }
+            for(int j=k+2;j<=n;j++){
+                cout<<j<<" ";
+            }
+            int x = k+1;
+            cout<<x<<"\n";
+        }
+    }
+}
