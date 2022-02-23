da39a12354d6572aa8307df4ff68f740b2252447
Ayush Bansal <bayush611@gmail.com>
2022-02-23 23:03:45 +0530

    Too many items

---
+++ CodeChef Starters 27/POLYBAGS.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int a;
+        cin>>a;
+
+        if(a%10 > 0){
+            cout<<a/10 + 1<<endl;
+        }
+        else{
+            cout<<a/10<<endl;
+        }
+    }
+}
