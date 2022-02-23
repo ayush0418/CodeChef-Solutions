271d92a8db3278064a4daaaa880dd3445e0368ac
Ayush Bansal <bayush611@gmail.com>
2022-02-23 23:02:55 +0530

    Distinct Binary Strings

---
+++ CodeChef Starters 27/BINSTRING.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include<string.h>
+#define ll long long int
+using namespace std;
+
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+
+        string s ;
+        cin>>s;
+        
+        int count = 1;
+        for(int i=0;i<n-1;i++){
+            if(s[i] != s[i+1]){
+                count++;
+            }
+        }
+        cout<<count<<endl;
+    }
+}
